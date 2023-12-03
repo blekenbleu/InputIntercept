fe55efdbcc28dd39ee2324c2017da3ac3d35f9e8
Steven Carmichael <blekenbleu@gmail.com>
2023-12-03 15:13:51 -0500

    update DPath for diagnostics;  null stream check avoids exception

--- InputInterceptor/Helpers.cs
+++ InputInterceptor/Helpers.cs
@@ -4,19 +4,28 @@ using System.Reflection;
 
 namespace InputInterceptorNS {
 
-    internal static class Helpers {
+	internal static class Helpers {
 
-        public static Byte[] GetResource(String name) {
-            TypeInfo typeInfo = typeof(Helpers).GetTypeInfo();
-            Assembly assembly = typeInfo.Assembly;
-            String path = typeInfo.Namespace + ".Resources." + name;
-            using (Stream stream = assembly.GetManifestResourceStream(path)) {
-                Byte[] result = new Byte[stream.Length];
-                stream.Read(result, 0, (Int32)stream.Length);
-                return result;
-            }
-        }
+		public static Byte[] GetResource(String name)
+		{
+			TypeInfo typeInfo = typeof(Helpers).GetTypeInfo();
+			Assembly assembly = typeInfo.Assembly;
+			InputInterceptor.DPath = typeInfo.Namespace + ".Resources." + name;
+			using (Stream stream = assembly.GetManifestResourceStream(InputInterceptor.DPath))
+			{
+				Byte[] result;
 
-    }
-
+				if(null == stream)
+				{
+					Console.WriteLine("InputInterceptor.Helpers.GetResource():  " +
+						InputInterceptor.DPath + " not found or invalid");
+					return null;
+				}
+		
+				result = new Byte[stream.Length];
+				stream.Read(result, 0, (Int32)stream.Length);
+				return result;
+			}
+		}
+	}
 }
